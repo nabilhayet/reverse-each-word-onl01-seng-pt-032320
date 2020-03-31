6a726db6d4de3e34b50364568844bf39364c14d7
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-31 02:48:20 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+
+def reverse_each_word(str)
+
+  ss=[]
+  array = str.split(" ")
+  array.each do |word|
+      ss << word.reverse
+  end
+  return ss.join(" ")
+end 
+
+##reverse_each_word("Hello there, and how are you?")
