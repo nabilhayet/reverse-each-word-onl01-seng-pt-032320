d6cc453276124ccc7d1f0510194e178ac62c590b
Nabil Hayet <nabil.bracu@gmail.com>
2020-03-31 02:50:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
-
+require "pry"
 def reverse_each_word(str)
   ss=[]
   array = str.split(" ")
   array.each do |word|
       ss << word.reverse
+      binding.pry
   end
   return ss.join(" ")
 end 
